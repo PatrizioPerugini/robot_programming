36cdbf8242c3afaea223e47700b97b216913e2ee
PatrizioPerugini <patrizioperugini99@gmail.com>
2022-01-16 15:11:58 +0100

    final robot model, integrated with ros -PP

---
+++ ws/build/robot_manipulator/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/patrizio/Desktop/robot_programming/ws/src/robot_manipulator
+# Build directory: /home/patrizio/Desktop/robot_programming/ws/build/robot_manipulator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
