888b14437af2106dc744619353d4a347ff9047ff
PatrizioPerugini <patrizioperugini99@gmail.com>
2022-01-17 22:52:46 +0100

    hope it works

--- ws/src/send_position/src/feeder.cpp
+++ ws/src/send_position/src/feeder.cpp
@@ -2,7 +2,7 @@
 #include "std_msgs/String.h"
 #include <sensor_msgs/JointState.h>
 #include <sstream>
-#include <util/delay.h>
+
 
 int main(int argc, char **argv)
 {
@@ -11,62 +11,30 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  //n.reset(new ros::NodeHandle(config->body()));
+
   ros::Publisher pub=n.advertise<sensor_msgs::JointState>("joint_states",1000);
-
-
-  //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
   ros::Rate loop_rate(10);
 
-   sensor_msgs::JointState msg;
-  // msg.position[5];
-   //msg.velocity[5];
-   //msg.effort[5];
+  sensor_msgs::JointState msg;
 
-
-  int count = 0;
-  while (ros::ok())
-  {
-    for(int i=-90; i<90; i++){
-      msg.position={20, 0, 0, 0, 0};
-      _delay_ms(10);
-    }  
-    
   
-  msg.velocity={0.2, 0.0, 0.0, 0.0, 0.0};
-   // msg.velocity[1]=0.0;
-   // msg.velocity[2]=0.0;
-   // msg.velocity[3]=0.0;
-   // msg.velocity[4]=0.0;
-//
-  msg.effort={1.0, 0.0, 0.0, 0.0, 0.0};
-   // msg.effort[1]=0.0;
-   // msg.effort[2]=0.0;
-   // msg.effort[3]=0.0;
-   // msg.effort[4]=0.0;
+  while (ros::ok()){
+    for(int i=-90; i<90; i++){
+      msg.position={(double)i, 0, 0, 0, 0};
+      msg.velocity={0.2, 0.0, 0.0, 0.0, 0.0};
+      msg.effort={1.0, 0.0, 0.0, 0.0, 0.0};
 
-//  position: [2.0, 0.0, 0.0, 0.0, 0.0]
-//velocity: [10.0, 0.0, 0.0, 0.0, 0.0]
-
-
-
- 
-
-//    std::stringstream ss;
-  //  ss << "is it publishing? " << count;
-   
-
-   // ROS_INFO("%f", msg.position[0]);
-    //ROS_INFO("%f", msg.velocity[1]);
-
-   
+      ros::Duration(0.05).sleep();
+      pub.publish(msg);
+    }
+  
     pub.publish(msg);
 
     ros::spinOnce();
 
     loop_rate.sleep();
-    ++count;
+  
   }
 
   return 0;
